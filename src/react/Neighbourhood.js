51003036245630a6a146bc21be278ef1815dc404
syltaxue <syltaxue@hotmail.com>
2021-05-05 23:07:35 -0700

    Rename neighbourhood part2

---
+++ src/react/Neighbourhood.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+class Neighbourhood extends React.Component {
+    render() {
+          return (
+                <div className="lg:mt-24">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                )
+        }
+};
+
+export default Neighbourhood;
